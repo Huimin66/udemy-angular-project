290a46f04229da619414e66c66ed56c49f39fc63
huimin <huim.cao@gmail.com>
2022-08-29 22:19:39 +0200

    Submit exercise3

--- src/app/exercise3/exercise3.component.ts
+++ src/app/exercise3/exercise3.component.ts
@@ -1,27 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core"
 
 @Component({
-  selector: 'app-exercise3',
-  templateUrl: './exercise3.component.html',
-  styleUrls: ['./exercise3.component.scss'],
-  // styles:[`
-  //   .greaterthanfive{
-  //     color:white
-  //   }
-  // `]
+  selector: "app-exercise3",
+  templateUrl: "./exercise3.component.html",
+  styleUrls: ["./exercise3.component.scss"],
+  styles: [
+    `
+      .greaterthanfive {
+        color: white;
+      }
+    `,
+  ],
 })
 export class Exercise3Component implements OnInit {
-  ifParaShowed:boolean = true
-  clickHistory:number[]= []
+  ifParaShowed: boolean = true
+  clickHistory: number[] = []
   clickTimes = 0
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  toggleDisplay(){
+  toggleDisplay() {
     this.clickHistory.push(++this.clickTimes)
     this.ifParaShowed = !this.ifParaShowed
   }
-
 }
